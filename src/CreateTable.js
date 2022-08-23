5f9f92e6b21cb8565c8c8f95f905c8e4af0af960
@karapetyan.tigran.1998 <karapetyan.tigran.1998@gmail.com>
2022-08-23 12:48:06 +0400

    in progress

--- src/CreateTable.js
+++ src/CreateTable.js
@@ -1,17 +1,21 @@
 // import "./Table.css"
 
-
 // function createTable()
 // {
-// let rn = window.prompt("Input number of rows", 1);
-  
-//  for(var r=0;r<rn;r++)
-//   {
+// let count = document.querySelector(".text").value;
+
+//  for(var r=0;r<count;r++)
+//  {
 //    var x=document.getElementById('myTable').insertRow(r);
-//    for(var c=0;c<rn;c++)  
-//     {
-//      var y=  x.insertCell(c);
-//      y.innerHTML="A"; 
-//     }
-//    }
+//   for(var c=0;c<count;c++)
+//    {
+//     var y=  x.insertCell(c);
+//     y.innerHTML="A";      }    }
 // }
+{
+  /* <table id="myTable" border="1"> 
+</table><form> 
+<input type = "text" class = "text">
+<input type="button" onclick="createTable()" value="Create the table"> 
+</form> */
+}
