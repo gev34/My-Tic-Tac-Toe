d8913e25d987bf41e6702611eaa2b9d070127471
@karapetyan.tigran.1998 <karapetyan.tigran.1998@gmail.com>
2022-08-30 12:38:00 +0400

    in progress

--- src/Choose.js
+++ src/Choose.js
@@ -19,56 +19,6 @@ function Choose(){
 }
 
     function PlayWithX() {
-
-      // function clickBox(num) {
-      //   console.log(num)
-      //         // if (turn === "o" && cellValue[num] === "" && winner !== "x") {
-      //         //   cellValue[num] = "o";
-      //         //   checkWinner();
-      //         //   //console.log(winner === "x");
-      //         //   setTurn("x");
-      //         //   random();    
-      //         // }
-          
-      //       }
-
-
-
-    
-      // function checkWinner() {
-      //   let allWiningCombs = [
-      //     [0, 1, 2],
-      //     [3, 4, 5],
-      //     [6, 7, 8],
-      //     [0, 3, 6],
-      //     [1, 4, 7],
-      //     [2, 5, 8],
-      //     [0, 4, 8],
-      //     [6, 4, 2],
-      //   ];
-    
-      //   allWiningCombs.forEach((winingComb) => {
-      //     if (
-      //       cellValue[winingComb[0]] === "x" &&
-      //       cellValue[winingComb[1]] === "x" &&
-      //       cellValue[winingComb[2]] === "x"
-      //     ) {
-      //       setWinner("x");
-      //     } else if (
-      //       cellValue[winingComb[0]] === "o" &&
-      //       cellValue[winingComb[1]] === "o" &&
-      //       cellValue[winingComb[2]] === "o"
-      //     ) {
-      //       setWinner("o");
-      //     }
-      //   });
-      // }
-    
-      // function restartGame() {
-      //   setWinner(null);
-      //   setCells(Array(9).fill(""));
-      //   setTurn("x");
-      // }
       return (
         <div>
         <MainPage />
@@ -122,15 +72,10 @@ function Choose(){
 
 
 
-       // console.log(cells)
 
         let winingCombs = [];
         let arr = Array.from(Array(val**2).keys());
-       // console.log(arr)
-        /* console.log(arr.slice(0,val))
-        console.log(arr.slice(val,val*2))
-        console.log(arr.slice(val*2,val*3))
-        console.log(arr.slice(val*3,val*val)) */
+
         for(let i = 1 ; i <= val ; i++){
           for(let j = i-1 ; j < i ; j ++){
             winingCombs.push(arr.slice(val*j,val*i))
@@ -154,19 +99,29 @@ function Choose(){
         winingCombs.push(arr2)
         winingCombs.push(arr3)
         winingCombs.push(arr4)
-        console.log(winingCombs)
 
-  
+
+        winingCombs.forEach((winingComb) => {
+              for(let i = 0 ; i < winingComb.length ; i++){
+                if (cells[winingComb[i]] === "x" ) {
+                    setWinner("x");
+                    alert('player x is the winner')
+              }
+              }
+            });
+          
 
         function clickBox(num){
          if(turn === 'x' && num.target.innerHTML === '' && winner !== 'o'){
           num.target.innerHTML = 'X';
+          cells[num.target.id] = 'x'
           setTurn("o");
         } else if(turn === 'o' && num.target.innerHTML === '' && winner !== 'x'){
           num.target.innerHTML = 'O';
+          cells[num.target.id] = 'o'
           setTurn("x");
         }
-      
+        console.log(cells)
         }
 
 
